a19d00513fad813865b1efaa69c543f34470c935
Jonathan Nicolas <jonathannicolas@Jonathans-MacBook-Pro.local>
2019-05-31 02:09:30 -0700

    Add ref to input and redirection to store

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,12 +1,28 @@
 import React, { Component } from "react";
+import { getFunName } from "../helpers";
 
 class StorePicker extends Component {
+  myInput = React.createRef()
+
+  gotToStore = (event) => {
+    event.preventDefault();
+    const storeName =this.myInput.current.value
+    this.props.history.push(`/store/${storeName}`)
+
+  }
+
   render() {
     return (
       <React.Fragment>
-        <form className="store-selector">
-          <h2>Hello</h2>
-          <input type="text" required placeholder="Store Name"></input>
+        <form className="store-selector" onSubmit={this.gotToStore}>
+          <h2>Please Enter A Store</h2>
+          <input
+            type="text"
+            ref={this.myInput}
+            required
+            placeholder="Store Name"
+            defaultValue={getFunName()}
+          />
           <button type="submit">Visit Store ➞ </button>
         </form>
       </React.Fragment>
