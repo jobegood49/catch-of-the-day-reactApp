bcd0ecf38e8c7668059245d578714127da45faaa
jobegood49 <jonathan.s.nicolas@gmail.com>
2019-06-01 21:11:22 -0700

    Add order component functionality

--- src/components/App.js
+++ src/components/App.js
@@ -22,6 +22,12 @@ class App extends Component {
       fishes: sampleFishes
     });
   };
+  addToOrder = key => {
+    const order = {...this.state.order}
+    order[key] = order[key] + 1 || 1
+    this.setState({order})
+
+  }
   render() {
     return (
       <div className="catch-of-the-day">
@@ -31,13 +37,13 @@ class App extends Component {
             {
               Object.keys(this.state.fishes).map(key => {
                 return(
-                  <Fish key={key} details={this.state.fishes[key]}>{key}</Fish>
+                  <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder}>{key}</Fish>
                 )
               })
             }
           </ul>
         </div>
-        <Order />
+        <Order fishes={this.state.fishes} order={this.state.order}/>
         <Inventory
           addFish={this.addFish}
           loadSampleFishes={this.loadSampleFishes}
